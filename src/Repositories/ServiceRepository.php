53fe8994f4b7763b175cc925b87cdb1773785fb5
leo108 <leo108@qq.com>
2016-09-20 10:04:58 +0800

    remove unused code

--- src/Repositories/ServiceRepository.php
+++ src/Repositories/ServiceRepository.php
@@ -8,7 +8,6 @@
 
 namespace Leo108\CAS\Repositories;
 
-use Leo108\CAS\Exceptions\UserException;
 use Leo108\CAS\Models\Service;
 use Leo108\CAS\Models\ServiceHost;
 
@@ -37,7 +36,7 @@ class ServiceRepository
 
     /**
      * @param $url
-     * @return \Leo108\CAS\Models\Service|null
+     * @return Service|null
      */
     public function getServiceByUrl($url)
     {
@@ -60,65 +59,5 @@ class ServiceRepository
         $service = $this->getServiceByUrl($url);
 
         return $service !== null && $service->enabled;
-    }
-
-    /**
-     * @param $name
-     * @param $hostArr
-     * @param $enabled
-     * @param $id
-     * @return \Leo108\CAS\Models\Service
-     */
-    public function createOrUpdate($name, $hostArr, $enabled = true, $id = 0)
-    {
-        \DB::beginTransaction();
-        if ($id == 0) {
-            if ($this->service->where('name', $name)->count() > 0) {
-                throw new UserException(trans('cas::message.service.name_duplicated'));
-            }
-
-            $service = $this->service->create(
-                [
-                    'name'       => $name,
-                    'enabled'    => boolval($enabled),
-                ]
-            );
-        } else {
-            $service          = $this->service->find($id);
-            $service->enabled = boolval($enabled);
-            $service->save();
-            $this->serviceHost->where('service_id', $id)->delete();
-        }
-
-        foreach ($hostArr as $host) {
-            $host = trim($host);
-            if ($this->serviceHost->where('host', $host)->count() > 0) {
-                throw new UserException(trans('cas::message.service.host_occupied', ['host' => $host]));
-            }
-            $hostModel = $this->serviceHost->newInstance(['host' => $host]);
-            $hostModel->service()->associate($service);
-            $hostModel->save();
-        }
-        \DB::commit();
-
-        return $service;
-    }
-
-    public function getList($search, $page, $limit)
-    {
-        /* @var \Illuminate\Database\Query\Builder $query */
-        $like = '%'.$search.'%';
-        if (!empty($search)) {
-            $query = $this->service->whereHas(
-                'hosts',
-                function ($query) use ($like) {
-                    $query->where('host', 'like', $like);
-                }
-            )->orWhere('name', 'like', $like)->with('hosts');
-        } else {
-            $query = $this->service->with('hosts');
-        }
-
-        return $query->orderBy('id', 'desc')->paginate($limit, ['*'], 'page', $page);
     }
 }
--- src/Utils/helpers.php
+++ src/Utils/helpers.php
@@ -20,18 +20,3 @@ if (!function_exists('cas_route')) {
         return route($name, $parameters, $absolute);
     }
 }
-
-if (!function_exists('cas_assets')) {
-    /**
-     * @param string $name
-     * @return string
-     */
-    function cas_assets($name)
-    {
-        if (config('cas.elixir')) {
-            return elixir($name);
-        }
-
-        return config('app.url').'/'.ltrim($name, '/');
-    }
-}
