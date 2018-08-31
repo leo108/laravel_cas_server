2eff77a480a82d1ecb5f52230baa35045fb4e8b9
rbayer <rbayer@sbts.edu>
2018-08-31 16:02:00 -0400

    fix names of new v3 routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -17,9 +17,9 @@ Route::group(
         Route::get('login', 'SecurityController@showLogin')->name($p.'login.get');
         Route::post('login', 'SecurityController@login')->name($p.'login.post');
         Route::get('logout', 'SecurityController@logout')->name($p.'logout')->middleware($auth);
-        Route::get('p3/login', 'SecurityController@showLogin')->name($p.'login.get');
-        Route::post('p3/login', 'SecurityController@login')->name($p.'login.post');
-        Route::get('p3/logout', 'SecurityController@logout')->name($p.'logout')->middleware($auth);
+        Route::get('p3/login', 'SecurityController@showLogin')->name($p.'v3.login.get');
+        Route::post('p3/login', 'SecurityController@login')->name($p.'v3.login.post');
+        Route::get('p3/logout', 'SecurityController@logout')->name($p.'v3.logout')->middleware($auth);
         Route::any('validate', 'ValidateController@v1ValidateAction')->name($p.'v1.validate');
         Route::any('serviceValidate', 'ValidateController@v2ServiceValidateAction')->name($p.'v2.validate.service');
         Route::any('proxyValidate', 'ValidateController@v2ProxyValidateAction')->name($p.'v2.validate.proxy');
